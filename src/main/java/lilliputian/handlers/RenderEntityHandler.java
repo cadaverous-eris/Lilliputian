6967a8c576bd0060e9822fe8aebacc8234e41ad7
machinespray <machinespray1@gmail.com>
2017-10-14 03:00:15 -0400

    Gui Change for 1.12.2

--- src/main/java/lilliputian/handlers/RenderEntityHandler.java
+++ src/main/java/lilliputian/handlers/RenderEntityHandler.java
@@ -7,6 +7,7 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -14,9 +15,12 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.util.ArrayList;
+
 @SideOnly(Side.CLIENT)
 @EventBusSubscriber(value = Side.CLIENT, modid = Lilliputian.MODID)
 public class RenderEntityHandler {
+	public static ArrayList<Multiplier> cache = new ArrayList<>();
 
 	@SubscribeEvent
 	public static void renderEntityPre(RenderLivingEvent.Pre event) {
@@ -67,6 +71,39 @@ public class RenderEntityHandler {
 				&& Minecraft.getMinecraft().gameSettings.thirdPersonView == 0) {
 			GlStateManager.translate(0, 0, -0.05F);
 			GlStateManager.translate(0, 0, (scale * 0.05F));
+		}
+	}
+
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	public static void renderGui(GuiScreenEvent.DrawScreenEvent.Post event) {
+		GlStateManager.disableBlend();
+		for(Multiplier m : cache){
+			GlStateManager.pushMatrix();
+			GlStateManager.scale(0.5,0.5,1);
+			GlStateManager.translate(event.getGui().width/(50-m.scale),-(event.getGui().height/m.scale),0);
+			Minecraft.getMinecraft().getRenderManager().getFontRenderer().drawString(m.string,m.x*2,m.y*2,553648127);
+			GlStateManager.popMatrix();
+		}
+		cache.clear();
+	}
+
+	@SideOnly(Side.CLIENT)
+	public static void registerMultiplier(int x, int y, String string,int scale){
+		cache.add(new Multiplier(x,y,string,scale));
+	}
+
+	private static class Multiplier {
+		public int x;
+		public int y;
+		public String string;
+		public int scale;
+
+		public Multiplier(int x, int y, String string,int scale) {
+			this.x=x;
+			this.y=y;
+			this.string=string;
+			this.scale=scale;
 		}
 	}
 
